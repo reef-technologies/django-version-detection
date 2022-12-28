7a51dbe2360b605f9d9fa2ecb719e902c763d4e2
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2022-12-28 17:44:51 +0300

    Update signatures

--- detect.py
+++ detect.py
@@ -1,15 +1,15 @@
-from argparse import ArgumentParser
-from pathlib import Path
 import json
-from typing import Optional
+import logging
+from argparse import ArgumentParser
 from hashlib import md5
+from pathlib import Path
+
 import requests
-from tabulate import tabulate
 from natsort import natsorted
+from tabulate import tabulate
 
-
+log = logging.getLogger(__name__)
 signatures = json.loads(Path('signatures.json').read_text())
-print('Loaded signatures for', ', '.join(signatures.keys()))
 
 
 def detect_version(domain: str, static_path: str = 'static/admin/') -> dict[str, float]:
@@ -22,21 +22,23 @@ def detect_version(domain: str, static_path: str = 'static/admin/') -> dict[str,
         static_path = domain + 'static/admin/'
     if not static_path.endswith('/'):
         static_path += '/'
-    print('Searching static files in', static_path)
+    log.info('Searching static files in %s', static_path)
 
     session = requests.Session()
     signature = {}  # current website signature
     for version, hashes in signatures.items():
-        print('Checking signatures for', version)
+        log.info('Getting signatures for %s', version)
         for file in hashes.keys():
-            if file not in signature:
-                url = static_path + file
-                print('Fetching', url)
-                try:
-                    response = session.get(url, timeout=5)
-                    signature[file] = md5(response.content).hexdigest() if response.ok else None
-                except Exception:
-                    signature[file] = None
+            if file in signature:
+                continue
+
+            url = static_path + file
+            log.debug('Fetching %s', url)
+            try:
+                response = session.get(url, timeout=5)
+                signature[file] = md5(response.content).hexdigest() if response.ok else None
+            except Exception:
+                signature[file] = None
 
     result = {}
     for version, version_signature in signatures.items():
@@ -47,12 +49,15 @@ def detect_version(domain: str, static_path: str = 'static/admin/') -> dict[str,
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
     parser = ArgumentParser()
     parser.add_argument('domain')
     parser.add_argument('--static-path', type=str, default='static/admin/')
     args = parser.parse_args()
 
-    print(args.domain)
+    log.info('Loaded signatures for %s', ', '.join(signatures.keys()))
+    log.info('Scanning %s', args.domain)
     versions = detect_version(args.domain, static_path=args.static_path)
     print(tabulate(
         reversed(natsorted((version, f'{likelihood*100:.2f}%') for version, likelihood in versions.items())),
